82fc4b4d60b3898426cfc306dca4dd4a2b1c81fe
AJIL K <86887880+ajil2545@users.noreply.github.com>
2021-12-23 12:09:08 +0530

    commit

--- DS LAB/Course Outcome 1/Program_13.c
+++ DS LAB/Course Outcome 1/Program_13.c
@@ -7,13 +7,13 @@ struct node
 	struct node *left;
 	struct node *right;
 };
-int n,i,item;
-struct node *p,*q,*root;
-/*inoredersuccessor(struct node *t)
+int n,i,item,k;
+struct node *p,*q,*root,*temp;
+void inoredersuccessor(struct node *t)
 {
 	if(t->right!=NULL)
 	{
-		temp=p->right;
+		temp=t->right;
 		while(temp->left!=NULL)
 		{
 			temp=temp->left;
@@ -22,58 +22,40 @@ struct node *p,*q,*root;
 	}
 	else
 	{
-		r=root;
-while(r->data<=r->data);
-	}
-}*/
-void createbst()
-{
-	printf("Enter the number of nodes\n");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	{
-		printf("Enter the node\n");
-		scanf("%d",&item);
-		p=(struct node *)(malloc(sizeof(struct node)));
-		p->data=item;
-		p->left=NULL;
-		p->right=NULL;
-		if(i==0)
+		struct node r=root, s;
+		while(r->data!=t->data)
 		{
-			root=p;
-		}
-		else
-		{
-			q=root;
-			while(1)
+			if(p->data<=r->data)
 			{
-		
-				if(p->data > q->data)
-				{
-					if(q->right==NULL)
-					{
-						q->right=p;
-						break;
-					}
-					else
-					{
-						q=q->right;
-					}	
-				}
-				else
-				{
-					if(q->left==NULL)
-					{
-						q->left=p;
-						break;
-					}
-					else
-					{
-						q=q->left;
-					}
-				}
+				s=r;
+				r=r->left;
 			}
+			else
+			{
+				r=r->right;
+			}
+			printf("%d is the inorder successor",s);
 		}
+	}
+}
+void search(struct node *t)
+{
+	
+	if(t->data==k)
+	{
+		printf("%d is Found",k);
+	}
+	else if(t->data>k && t->left!=NULL)
+	{
+		search(t->left);
+	}
+	else if(t->data<k && t->right!=NULL)
+	{
+		search(t->right);
+	}
+	else
+	{
+		printf("%d is not Found",k);
 	}
 }
 void preorder(struct node *t)
@@ -125,33 +107,82 @@ void maximum(struct node *t)
 void main()
 {
 	int c=1,option;
+	printf("Enter the number of nodes\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("Enter the node\n");
+		scanf("%d",&item);
+		p=(struct node *)(malloc(sizeof(struct node)));
+		p->data=item;
+		p->left=NULL;
+		p->right=NULL;
+		if(i==0)
+		{
+			root=p;
+		}
+		else
+		{
+			q=root;
+			while(1)
+			{
+		
+				if(p->data > q->data)
+				{
+					if(q->right==NULL)
+					{
+						q->right=p;
+						break;
+					}
+					else
+					{
+						q=q->right;
+					}	
+				}
+				else
+				{
+					if(q->left==NULL)
+					{
+						q->left=p;
+						break;
+					}
+					else
+					{
+						q=q->left;
+					}
+				}
+			}
+		}
+	}
 	printf("\n**** Binary Search Tree Operation****\n");
 	while(c==1)
 	{
 		printf("\n**** Main Menu ****\n");
-		printf("1. Create BST\n");
+		printf("1. Search\n");
 		printf("2. Preorder\n");
 		printf("3. Inorder\n");
-		printf("3. Postorder\n");
-		printf("4. Minimum value\n");
-		printf("4. Maximum value\n");
+		printf("4. Postorder\n");
+		printf("5. Minimum value\n");
+		printf("6. Maximum value\n");
 	    	printf("\nEnter your option : ");
 	    	scanf("%d",&option);
 		switch(option)
 		{
-			case 1:createbst();
+			case 1: printf("\nEnter the data to be searched: ");
+				scanf("%d",&k);
+				search(root);
 				break;
-			case 2:preorder(root);
+			case 2: preorder(root);
 				break;
-			case 3:inorder(root);
+			case 3: inorder(root);
 				break;
-			case 4:postorder(root);
+			case 4: postorder(root);
 				break;
-			case 5:minimum(root);
+			case 5: minimum(root);
 				break;
-			case 6:maximum(root);
+			case 6: maximum(root);
 				break;
-			default:printf("\nInvalid Option");
+			default: printf("\nInvalid Option");
 		}
 		printf("\nDo you want to continue(0/1) : ");
 		scanf("%d",&c);
